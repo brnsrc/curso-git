760fc92b0aaf04f7ed84fe565b4a08b1c3066e02
Bruno Souza <brunosrc@hotmail.com>
2020-02-06 21:10:53 -0300

    Acrescentada msg desejando uma boa noite

---
+++ projeto-teste/Dog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projeto_teste
+{
+    class Dog
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace projeto_teste
 {
@@ -6,7 +9,30 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7};
+            //var impares = list.FindAll(x => x % 2 == 1);
+
+            //foreach (var item in impares)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            List<Dog> dogs = new List<Dog>()
+            {
+                new Dog{Name = "Rex", Age = 4},
+                new Dog{Name = "Sean", Age = 0},
+                new Dog{Name = "Stacy", Age =3}
+            };
+
+            //var names = dogs.Select(x => x.Name);
+            //var newDogList = dogs.Select(x => new { Age = x.Age, FirstLetter = x.Name[0] });
+            var sortedDogs = dogs.OrderByDescending(x => x.Age);
+            foreach (var item in sortedDogs)
+            {
+                Console.WriteLine(string.Format($"Dog {item.Name} is {item.Age} years old."));
+            }
+
+            Console.WriteLine("Tenhamos uma boa noite !");
         }
     }
 }
